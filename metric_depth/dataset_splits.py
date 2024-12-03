1725722239e0024213707339f02d78de65c9e8c7
David NUC <strdav029@myuct.ac.za>
2024-12-03 11:25:08 +0200

    Create dataset_splits.py script

---
+++ metric_depth/dataset_splits.py
@@ -0,0 +1,16 @@
+import argparse
+import splitfolders
+
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-o", "--output_dir", type=str, default='/home/sil/data/farm_monos/splits')
+    parser.add_argument("-i", "--input_dir", type=str,default='/home/sil/data/farm_monos/image/')
+    parser.add_argument("-r", "--ratio", type=tuple, default=(.8, 0.1, 0.1))
+    parser.add_argument("-s", "--seed", type=int, default=42)
+    args, unknown_args = parser.parse_known_args()
+    print(f'Splitting dataset from {args.input_dir}')
+    splitfolders.ratio(input=args.input_dir, output=args.output_dir, seed=args.seed, ratio=args.ratio)
+    print(f'Split dataset with ratio {args.ratio} created in {args.output_dir}')
+
+if __name__ == '__main__':
+    main()
