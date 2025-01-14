3ed3973483a3612530e0c864ef8c18cfb26b993b
David NUC <strdav029@myuct.ac.za>
2025-01-14 15:10:05 +0200

    Fix wandb log issue

--- metric_depth/zoedepth/trainers/base_trainer.py
+++ metric_depth/zoedepth/trainers/base_trainer.py
@@ -297,16 +297,16 @@ class BaseTrainer:
             except AttributeError:
                 min_depth = None
                 max_depth = None
-        print(f'\nGT Avg: {torch.mean(depth["GT"]).item():.2f}')
-        print(f'Max: {torch.max(depth["GT"]).item():.2f}')
+        print(f'\nGT Avg: {torch.mean(depth["GT"]).item():.2f} Max: {torch.max(depth["GT"]).item():.2f}')
         print(f'\nPredMono Avg: {torch.mean(depth["PredictedMono"]).item():.2f} Max: {torch.max(depth["PredictedMono"]).item():.2f}')
         # depth = {k: colorize(v, vmin=min_depth, vmax=max_depth)
         #          for k, v in depth.items()}
         # scalar_field = {k: colorize(v, vmin=None, vmax=None, cmap=scalar_cmap) for k, v in scalar_field.items()}
         images = {**rgb, **depth, **scalar_field}
         # Specify 'I;16' uint16 when saving the depth images or None (default val) for RGB image
-        wimages = {prefix+"Predictions": [ wandb.Image(v, caption=k, mode = None if 'Input' in k else 'I;16')
-                                          for k, v in images.items()]}
+        # wimages = {prefix+"Predictions": [wandb.Image(v, caption=k, mode = None if 'Input' in k else 'I;16')
+        #                                   for k, v in images.items()]}
+        wimages = {prefix+"Predictions": [wandb.Image(v, caption=k, mode='I;16') for k, v in images.items()]}
         wandb.log(wimages, step=self.step)
 
     def log_line_plot(self, data):
