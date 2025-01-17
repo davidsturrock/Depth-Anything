ba5bc24bd5c8e058127c285b7a46e0a539a149de
David NUC <strdav029@myuct.ac.za>
2025-01-17 15:03:50 +0200

    HPC data dir

--- metric_depth/zoedepth/utils/config.py
+++ metric_depth/zoedepth/utils/config.py
@@ -35,7 +35,7 @@ ROOT = pathlib.Path(__file__).parent.parent.resolve()
 
 # HOME_DIR = os.path.expanduser("~/data")
 HOME_DIR = "/scratch/01475322/data"
-HP
+
 COMMON_CONFIG = {
     # "save_dir": os.path.expanduser("./depth_anything_finetune"),
     "save_dir": "/scratch/01475322/depth_anything_finetune",
